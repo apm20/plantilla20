e4435acd12d22c43a55253420fdd2f0606790430
apm20 <apm20@hotmail.com>
2014-10-01 00:42:11 +0200

    Update marcar.php

--- listas/marcar.php
+++ listas/marcar.php
@@ -44,7 +44,7 @@ foreach ( $javascript as $fic_js ) { ?>
 
 <p></p>
 
-	<button class="btn btn-default"><i class="glyphicon glyphicon-chevron-left"> </i> <?php echo $link_back; ?></button>
+	<button class="btn btn-default btn-circle btn-lg"><i class="fa fa-chevron-left"> </i> <?php $link_back; ?></button>
     <button class='btn btn-success btn-circle btn-lg' type='submit' name='btn_ok' id='btn_ok' onclick="this.style.visibility = 'hidden';" /><i class="fa fa-floppy-o"></i></button>
     <input class='btn btn-default' type='button' name='btn_todo_marcar' id='btn_todo_marcar' value="Marcar Todos" onclick="javascript:seleccionar_todo()" />
     <input class='btn btn-default' type='button' name='btn_todo_desmarcar' id='btn_todo_desmarcar' value="Desmarcar Todos" onclick="javascript:deseleccionar_todo()" />
