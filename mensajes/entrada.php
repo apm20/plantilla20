c7fd25b1318b92aff655fd70855db2dace169b3b
cromosrepes <cromosrepes@gmail.com>
2014-10-01 20:25:58 +0200

    Update entrada.php

--- mensajes/entrada.php
+++ mensajes/entrada.php
@@ -105,13 +105,43 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
   
 
 
-<?php
 function enlaces ( $estado ) {
 	echo 
-	anchor  ('mensajes/menu/'				,'<button class="btn btn-default"><i class="glyphicon glyphicon-chevron-left"> </i> Volver</button>'		,array('class'=>'back')).'  '.
+	anchor  ('mensajes/menu/'		,'<button class="btn btn-default"><i class="glyphicon glyphicon-chevron-left"> </i> Volver</button>'		,array('class'=>'back')).'  '.
 	anchor  ('seleccion/buscar/mensajes'	,'<button class="btn btn-success"><i class="glyphicon glyphicon-plus"> </i> Escribir mensaje nuevo</button>',array('class'=>'back')).'  '.
-	anchor  ('mensajes/enviados/'			,'<button class="btn btn-default"><i class="glyphicon glyphicon-send"> </i> Enviados</button>'		,array('class'=>'back'));
-	if ( $estado != 0 )
+	anchor  ('mensajes/enviados/'		,'<button class="btn btn-default"><i class="glyphicon glyphicon-send"> </i> Enviados</button>'		,array('class'=>'back'));
+
+    echo "<div class='row'>";
+    echo '<ul class="nav nav-tabs" role="tablist">';
+    echo '<li ';
+    if ( $estado == 0 ) echo 'class="active"';
+    echo '>'.anchor('mensajes/entrada/0/0'		,'<i class="glyphicon glyphicon-inbox"></i> Buzón de entrada',array('class'=>'back')); 
+    echo '</li>';
+    echo '<li ';
+    if ( $estado == 2 ) echo 'class="active"';
+    echo '>'.anchor('mensajes/entrada/0/2'		,'<i class="glyphicon glyphicon-transfer"> </i> Negociando',array('class'=>'back'));	
+    echo '</li>';
+    echo '<li ';
+    if ( $estado == 4 ) echo 'class="active"';
+    echo '>'.anchor('mensajes/entrada/0/4'		,'<i class="glyphicon glyphicon-time"> </i> Esperando',array('class'=>'back'));
+    echo '</li>';
+    echo '<li ';
+    if ( $estado == 1 ) echo 'class="active"';
+    echo '>'.anchor('mensajes/entrada/0/1'		,'<i class="glyphicon glyphicon-remove"> </i> Borrados',array('class'=>'back'));
+    echo '</li>';
+    echo '<li ';
+    if ( $estado == 'S' ) echo 'class="active"';
+    echo '>'.anchor('mensajes/entrada/0/S'		,'<i class="glyphicon glyphicon-ban-circle"> </i> No deseado',array('class'=>'back'));
+    echo '</li>';
+    echo '<li ';
+    if ( $estado == 'S' ) echo 'class="active"';
+    echo '>'.anchor (array ( 'social','ListaNegra' ),'<button class="btn btn-danger"><i class="glyphicon glyphicon-eye-close"> </i>Ver lista de ignorados</button>',array('class'=>'back'));
+    echo '</li>';
+    
+    
+/*    
+    
+	if ( $estado != 0 ) 
 		echo '  '.anchor('mensajes/entrada/0/0'		,'<button class="btn btn-default"><i class="glyphicon glyphicon-inbox"> </i> Buzón de entrada</button>',array('class'=>'back'));
 	if ( $estado != 2 )
 		echo '  '.anchor('mensajes/entrada/0/2'		,'<button class="btn btn-default"><i class="glyphicon glyphicon-transfer"> </i> Negociando</button>',array('class'=>'back'));	
@@ -123,9 +153,12 @@ function enlaces ( $estado ) {
 		echo '  '.anchor('mensajes/entrada/0/S'		,'<button class="btn btn-default"><i class="glyphicon glyphicon-ban-circle"> </i> No deseado</button>',array('class'=>'back'));
 	else
 		echo '  '.anchor (array ( 'social','ListaNegra' ),'<button class="btn btn-danger"><i class="glyphicon glyphicon-eye-close"> </i>Ver lista de ignorados</button>',array('class'=>'back'));
-	
-		
+*/
+
+echo '</ul>';		
+echo "</div>";
 }		
 ?>
+
 
 </center>
