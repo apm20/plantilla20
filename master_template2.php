ff15918c975eaf37761711bacc31751c42c90b85
apm20 <apm20@hotmail.com>
2014-10-01 12:49:00 +0200

    Update master_template2.php

--- master_template2.php
+++ master_template2.php
@@ -114,11 +114,11 @@
                             <!-- /.nav-second-level -->
                         </li>
                     </ul>
-<br/>
+
                     <!-- Adsense Lateral Gordo 160x600 recomendacion ini -->
                     <div class="row text-center">
-                    
-                        <div class="text-right hidden-xs col-lg-12">
+                    <br/>
+                        <div class="hidden-xs col-lg-12">
 
                             <!-- Adsense Lateral Gordo 160x600 recomendacion inicio -->
                             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
