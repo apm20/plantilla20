d383ee1b46fcfd2abdaa81e8663937a22093dfff
apm20 <apm20@hotmail.com>
2014-10-01 16:00:43 +0200

    Update nick.php

--- listas/nick.php
+++ listas/nick.php
@@ -128,10 +128,12 @@ if ($finalizadas== 0) {
 <?php echo  enlaces( $link_back, $usuario->nick_mostrar, $visitante  ) ?>
 <?php
 function enlaces( $link_back, $nick_mostrar, $p_visitante  ) {?>
-<?php echo $link_back, "<button class='btn btn-default'><i class='fa fa-chevron-left'> </i> Volver</button>";?>
+<?php 
+echo anchor("mensajes/menu/ <button class='btn btn-default'> Volver</button>");
+?>
 
 <?php if ($p_visitante)
-		echo anchor("mensajes/nuevo/".$nick_mostrar, "<button class='btn btn-default'><strong> Enviar mensaje a ".$nick_mostrar,"</strong></button>");
+		echo anchor("mensajes/nuevo/".$nick_mostrar, "<button class='btn btn-default'><strong> Enviar mensaje</strong></button>");
 		
 	
 }
