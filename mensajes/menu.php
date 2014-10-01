787d326e4578ca080605877e36f4dc0bdc3eb24e
cromosrepes <cromosrepes@gmail.com>
2014-10-01 19:26:29 +0200

    Update menu.php

--- mensajes/menu.php
+++ mensajes/menu.php
@@ -8,49 +8,34 @@ Aqu&iacute; puedes escribir nuevos mensajes y ver quien te ha escrito.
 <div class="text-center">
  <div class="btn-group-vertical ">
   <div class="btn-group">
-   <p>
-        <?php echo  anchor("mensajes/entrada/0/0" ,
-                "<button class='btn btn-default btn-block'><i class='fa pull-left fa-inbox'></i> <span class='pull-left'>".
-                "Buz&oacute;n de entrada ".$estilo_class->PintaPendientes ($pendientes).
-                "</span></button>",
-		'title="Aqu&iacute; llegan todos los mensajes que te escriban"') ?>
-   </p><p>
-        <?php echo  anchor("mensajes/entrada/0/2" ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-comments-o'></i> <span class='pull-left'>".
-        "Carpeta Negociando ".
-        "</span></button>",
-  		'title="Aqu&iacute; puedes guardar mensajes que te interese negociar"' ) ?>
-   </p><p>
-        <?php echo  anchor("mensajes/entrada/0/4" ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-clock-o'></i> <span class='pull-left'>".
-        "Carpeta Esperando ".
-        "</span></button>",
-  		'title="Aqu&iacute; puedes guardar mensajes de env&iacute;os que estes esperando a que te lleguen"' ) ?>
-   </p><p>
-        <?php echo  anchor("mensajes/enviados"    ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-send'></i> <span class='pull-left'>".
-        "Enviados".
-        "</span></button>",
-  		'title="Aqu&iacute; puedes consultar los mensajes que has enviado a otros usuarios"' ) ?>
-   </p><p>
-        <?php echo  anchor("mensajes/entrada/0/1" ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-remove'></i> <span class='pull-left'>".
-        "Borrados".
-        "</span></button>",
-  		'title="Aqu&iacute; puedes encontrar mensajes que borraste, por si quieres recuperarlos"' ) ?>
-   </p><p>
-        <?php echo  anchor("mensajes/entrada/0/S" ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-trash'></i> <span class='pull-left'>".
-        "Correo no deseado".
-        "</span></button>",
-  		'title="Aqu&iacute; se archivan los mensajes que te han enviado los usuarios de tu lista negra"' ) ?>
-   </p><p>
-        <?php echo  anchor("seleccion/buscar/mensajes"  ,
-        "<button class='btn btn-default btn-block'><i class='fa pull-left fa-plus'></i> <span class='pull-left'>".
-        "Escribir nuevo mensaje".
-        "</span></button>",
-  		'title="Escribe un nuevo mensaje al usuario que desees"' ); ?>
-   </p>
+    <p> <?php PintaBoton ("mensajes/entrada/0/0","fa-inbox",
+                           "Buz&oacute;n de entrada ".$estilo_class->PintaPendientes ($pendientes),
+                           "title='Aqu&iacute; llegan todos los mensajes que te escriban'"); ?>
+    </p><p> <?php PintaBoton ("mensajes/entrada/0/2" ,"fa-comments-o",
+                       "Carpeta Negociando ",
+                      'title="Aqu&iacute; puedes guardar mensajes que te interese negociar"'
+        ); ?> 
+    </p><p> <?php PintaBoton ("mensajes/entrada/0/4" ,"fa-clock-o",
+                      "Carpeta Esperando ",
+                      'title="Aqu&iacute; puedes guardar mensajes de env&iacute;os que estes esperando a que te lleguen"'
+        ); ?> 
+    </p><p> <?php PintaBoton ("mensajes/enviados"    ,"fa-send",
+                       "Enviados ",
+                      'title="Aqu&iacute; puedes consultar los mensajes que has enviado a otros usuarios"' 
+        ); ?> 
+    </p><p> <?php PintaBoton ("mensajes/entrada/0/1" ,"fa-remove",
+                      "Borrados",
+                      'title="Aqu&iacute; puedes encontrar mensajes que borraste, por si quieres recuperarlos"'
+        ); ?> 
+    </p><p> <?php PintaBoton ("mensajes/entrada/0/S" ,"fa-trash",
+                      "Correo no deseado",
+                      'title="Aqu&iacute; se archivan los mensajes que te han enviado los usuarios de tu lista negra"'
+        ); ?> 
+    </p><p> <?php PintaBoton ("seleccion/buscar/mensajes"  ,"fa-plus",
+                      "Escribir nuevo mensaje",
+                      'title="Escribe un nuevo mensaje al usuario que desees"'
+        ); ?> 
+    </p>
   </div>
  </div>
 </div>
@@ -69,7 +54,15 @@ Aqu&iacute; puedes escribir nuevos mensajes y ver quien te ha escrito.
 </script>
 <?php  } ?>
 
-
+<?php
+function PintaBoton ( $url, $icono, $texto, $title ) {
+       echo  anchor( $url ,
+                "<button class='btn btn-default btn-block'><i class='fa pull-left ".$icono."'></i> <span class='pull-left'>".
+                $texto.
+                "</span></button>",
+		        $title);
+}
+?>
 
 <p>Los mensajes se guardan dos meses antes de ser borrados.</p>
 <p>Si tienes m&aacute;s de 20 mensajes pendientes de lectura no aparecer&aacute;s en los resultados del buscador</p>
