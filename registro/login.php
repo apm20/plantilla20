403f2a3fbfdd864db803ecc05e50c80be7937689
apm20 <apm20@hotmail.com>
2014-10-02 00:45:07 +0200

    Update login.php

--- registro/login.php
+++ registro/login.php
@@ -15,33 +15,43 @@
     	document.write( '<input type="hidden" name="ad" id="ad" value="0"/>');
     }
 </script>	
-<center><div class="col-md-3">
-<div class="form-group">
-  
-  <input type="text" name="nick" id="nick" class="form-control" placeholder="Usuario" autofocus onclick="this.focus();this.select()" 
+<center>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input type="text" name="nick" id="nick" class="form-control" autofocus placeholder="Usuario" autofocus onclick="this.focus();this.select()" 
         	value="<?php echo $nick ?>" /></td><td><?php echo form_error('nick')?>
-        	
-        	</div>
-  <div class="form-group">
-  <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" onclick="this.focus();this.select()" 
+                                </div>
+                                <div class="form-group">
+                                 <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" onclick="this.focus();this.select()" 
         	value="<?php echo $password?>" /><td><?php echo form_error('password')?>
-</div>
-
-<div class="checkbox">
-    <label>
-      <input type="checkbox"name="recuerdame" value="1"
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                         <input type="checkbox"name="recuerdame" value="1"
 		<?php if ($recuerdame==1) echo "checked"; ?> > Recuérdame
-    </label>
-  </div>
-  
-  
-  <input type="submit" name="submit" id="submit" value="Iniciar sesión" class="btn btn-success btn-lg btn-outline"  />
-</div></center>
-<p></p>
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                 <input type="submit" name="submit" id="submit" value="Iniciar sesión" class="btn btn-lg btn-success btn-block">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-	
-
-    
     </form>
 </fieldset>
 
