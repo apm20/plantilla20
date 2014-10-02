9179e9ad250e017b147df284dbc1404f82d19580
cromosrepes <cromosrepes@gmail.com>
2014-10-02 18:09:40 +0200

    Update entrada.php

--- mensajes/entrada.php
+++ mensajes/entrada.php
@@ -37,7 +37,9 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 <h1><i class='fa fa-envelope-o'></i> <?php echo $title ?></h1>
 <?php enlaces( $estado ); ?>
 <center>
-<?php if ( $cuantos == 0) { echo "<p>El buzón está vacío</p>"; } else { ?>
+<?php 
+$cuantos = count(encabezados);
+if ( $cuantos == 0) { echo "<p>El buzón está vacío</p>"; } else { ?>
 <table class="table table-bordered table-hover">
 <thead>
     <tr align="center" class="active">
