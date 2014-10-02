f91a8a50ee930fe372f97cb0ab6d4490d18be810
apm20 <apm20@hotmail.com>
2014-10-02 18:47:50 +0200

    Update entrada.php

--- mensajes/entrada.php
+++ mensajes/entrada.php
@@ -91,8 +91,8 @@ if ( $cuantos == 0) { echo "<p></p>El buzón está vacío"; } else { ?>
 	if ( $pagina >0)
 		echo anchor  ('mensajes/entrada/'.(intval($pagina)-1).'/'.$estado,' <button class="btn btn-default"><i class="fa fa-chevron-left"> </i>  Anterior</button>',array('class'=>'back'));
 ?>
-<?php if ( $cuantos == 0)
-echo "Página" .$pagina+1; ?>
+<?php if ( $cuantos != 0)
+echo " Página" .$pagina+1. " "; ?>
 <?php
       	if ( $cuantos >20)
 			echo anchor  ('mensajes/entrada/'.(intval($pagina)+1).'/'.$estado,'<button class="btn btn-default">Siguiente <i class="fa fa-chevron-right"> </i></button> ',array('class'=>'back'));
