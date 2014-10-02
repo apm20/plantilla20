53b1157e37b89778a7bdf0963356e05b37f95db6
apm20 <apm20@hotmail.com>
2014-10-02 19:54:19 +0200

    Update detalle.php

--- listas/detalle.php
+++ listas/detalle.php
@@ -3,8 +3,7 @@
 <style type="text/css">
 	textarea{resize:none;}
 </style>
-<?php echo anchor('listas/nick/' .$usuario->nick_mostrar, '<button class="btn btn-default"><i class="fa fa-chevron-left"> </i> Volve</button>');?>
-<?php echo $link_back;?>
+<?php echo anchor('listas/nick/' .$usuario->nick_mostrar, '<button class="btn btn-default"><i class="fa fa-chevron-left"> </i> Volver</button>');?>
 <?php echo  enlaces( $visitante,$coleccion, $autor,$usuario->nick_mostrar,$usuario->id  ) ?>
 <br />    
 <p>
