30e954cd5177a6902652b0039ed6cd68329ff7e4
apm20 <apm20@hotmail.com>
2014-10-02 18:03:28 +0200

    Update entrada.php

--- mensajes/entrada.php
+++ mensajes/entrada.php
@@ -72,14 +72,14 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 		<?php $cuantos ++; ?>
         </td></tr>
     <?php if ( $row["leido"]=='0' ) echo "</strong>" ?>        
-
+<?php endforeach; ?>
 
 
 
 </tbody>
 
 </table>
-<?php endforeach; ?>
+
 <?php if ( $cuantos == 0) { echo "<p>El buzón está vacío</p>"; } else { ?>
 <?php } // if ( $cuantos == 0) ?>
 <p></p>
