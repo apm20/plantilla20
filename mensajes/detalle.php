504c370356bee6b82c0be9fda3558a6171e606f8
apm20 <apm20@hotmail.com>
2014-10-02 15:52:23 +0200

    Update detalle.php

--- mensajes/detalle.php
+++ mensajes/detalle.php
@@ -37,7 +37,7 @@ var t_ini = new Date();
 <?php
 function enlaces ( $enviados, $usuario, $mensaje, $url_back_esc ) {
 
-	echo anchor (array ( 'mensajes','Nuevo',$usuario->nick_mostrar,$mensaje[0]["id"]),	'<button class="btn btn-default">Responder</button>',array('class'=>'back')) ;
+	echo anchor (array ( 'mensajes','Nuevo',$usuario->nick_mostrar,$mensaje[0]["id"]),	'<div class="btn-group"><button class="btn btn-default">Responder</button>',array('class'=>'back')) ;
 
 	if ($enviados==false) {
 		 echo anchor (array ( 'mensajes','Nuevo',$usuario->nick_mostrar,$mensaje[0]["id"],1),'<button class="btn btn-success btn-outline">Aceptar Cambio</button> ',array('class'=>'back')) ;
@@ -96,7 +96,7 @@ function enlaces ( $enviados, $usuario, $mensaje, $url_back_esc ) {
 		// Si es un mensaje de Spam no podemos meter al usuario en lista negra
 		echo anchor (array ( 'social','ListaNegraCrear',
 					$usuario->nick ),
-			'<button class="btn btn-default">Ignorar Usuario',array('class'=>'back')). ' </button> ' ;
+			'<button class="btn btn-default">Ignorar Usuario',array('class'=>'back')). ' </button> </div>' ;
 	}
 
 
